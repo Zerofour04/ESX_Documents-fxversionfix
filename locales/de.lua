b2f67d948259dbbea6b0a5301295c1ef55e1fd51
Zerofour04 <brubiyt@gmail.com>
2021-02-13 13:13:13 +0100

    FIXED: wrong fx_version | Codecleanup | extended german translation
    Hey, I fork the repository to fix the problems:
    -FIXED: Resource ESX_Documents does not specify an fx_version in fxmanifest.lua.
    -Extended german translation
    -Codecleanup
    -Replaced the white paper. It's cleaner.

--- locales/de.lua
+++ locales/de.lua
@@ -21,23 +21,23 @@ Locales['de'] = {
 Config.Documents['de'] = {
       ["public"] = {
         {
-          headerTitle = "BESTÄTIGUNGSFORMULAR",
-          headerSubtitle = "Formular zur Bestätigung",
+          headerTitle = "Bestaetigungsformular",
+          headerSubtitle = "Formular zur Bestaetigung",
           elements = {
-            { label = "BESTÄTIGUNGS GEGENSTAND", type = "textarea", value = "", can_be_emtpy = false },
+            { label = "Gegendstand", type = "textarea", value = "", can_be_emtpy = false },
           }
         },
         {
           headerTitle = "Zeugenaussage",
           headerSubtitle = "Rechtlich verwertbare Zeugenaussage",
           elements = {
-            { label = "ZEITPUNKT", type = "input", value = "", can_be_emtpy = false },
-            { label = "VORFALL SCHILDERUNG", type = "textarea", value = "", can_be_emtpy = false },
+            { label = "Zeitpunkt", type = "input", value = "", can_be_emtpy = false },
+            { label = "Vorfall-Schilderung", type = "textarea", value = "", can_be_emtpy = false },
           }
         },
         {
-          headerTitle = "FAHRZEUG ÜBERGABE FORMULAR",
-          headerSubtitle = "KFZ Übergabeformular",
+          headerTitle = "Fahrzeug Uebergabeformular",
+          headerSubtitle = "KFZ Uebergabeformular",
           elements = {
             { label = "Kennzeichen", type = "input", value = "", can_be_emtpy = false },
             { label = "Name", type = "input", value = "", can_be_emtpy = false },
@@ -46,113 +46,113 @@ Config.Documents['de'] = {
           }
         },
         {
-          headerTitle = "SCHULDENERKLÄRUNG",
-          headerSubtitle = "Schuldanerkennung gegenüber einem anderen Bürger.",
+          headerTitle = "Schuldenerklaerung",
+          headerSubtitle = "Schuldanerkennung gegenueber einem anderen Buerger.",
           elements = {
             { label = "Schuldner Vorname", type = "input", value = "", can_be_emtpy = false },
             { label = "Schuldner Nachname", type = "input", value = "", can_be_emtpy = false },
             { label = "Betrag", type = "input", value = "", can_be_empty = false },
-            { label = "Zurückzuzahlen bis", type = "input", value = "", can_be_empty = false },
+            { label = "Zurueckzuzahlen bis", type = "input", value = "", can_be_empty = false },
             { label = "Zusatzangaben", type = "textarea", value = "", can_be_emtpy = true },
           }
         },
         {
-          headerTitle = "SCHULD RÜCKZAHLUNGS ERKLÄRUNG",
-          headerSubtitle = "Entschuldungserklärung eines anderen Bürgers.",
+          headerTitle = "Schuld Rueckzahlungserklaerung",
+          headerSubtitle = "Entschuldungserklaerung eines anderen Buergers.",
           elements = {
             { label = "Vorname", type = "input", value = "", can_be_emtpy = false },
             { label = "Nachname", type = "input", value = "", can_be_emtpy = false },
             { label = "Betrag", type = "input", value = "", can_be_empty = false },
-            { label = "Erklärung", type = "textarea", value = "Hiermit erkläre ich, dass der zuvor genannte Bürger eine Zahlung mit dem zuvor genannten Schuldenbetrag abgeschlossen hat", can_be_emtpy = false, can_be_edited = false },
+            { label = "Erklaerung", type = "textarea", value = "Erklaerung", can_be_emtpy = false, can_be_edited = false },
           }
         }
       },
       ["police"] = {
         {
-          headerTitle = "PARK ERLAUBNIS",
+          headerTitle = "Parkgenehmigung",
           headerSubtitle = "Park Sondergehnehmigung",
           elements = {
             { label = "Vorname", type = "input", value = "", can_be_emtpy = false },
             { label = "Nachname", type = "input", value = "", can_be_emtpy = false },
-            { label = "Gültig bis", type = "input", value = "", can_be_empty = false },
-            { label = "Sondergehnehmigung", type = "textarea", value = "DER EINGETRAGENE BÜRGER HAT IN JEDER STADTZONE EINE UNBEGRENZTE PARKERLAUBNIS GEWÄHRT UND DIESE IST BIS ZUM VORGESEHENEN DATUM GÜLTIG.", can_be_emtpy = false },
+            { label = "Gueltig bis", type = "input", value = "", can_be_empty = false },
+            { label = "Sondergehnehmigung", type = "textarea", value = "Der eingetragene Buerger hat eine Genehmigung zum Parken bis zum vorgesehenen Datum. Ort:", can_be_emtpy = false },
           }
         },
         {
-          headerTitle = "ZUSATZ WAFFENSCHEIN",
-          headerSubtitle = "Waffenerlaubnis für Spezielle Waffen.",
+          headerTitle = "Waffenschein-Waffenbesitzkarte",
+          headerSubtitle = "Waffenerlaubnis für spezielle Waffen.",
           elements = {
             { label = "Vorname", type = "input", value = "", can_be_emtpy = false },
             { label = "Nachname", type = "input", value = "", can_be_emtpy = false },
-            { label = "Gültig bis", type = "input", value = "", can_be_empty = false },
-			{ label = "Waffe", type = "input", value = "", can_be_empty = false },
-            { label = "INFORMATION", type = "textarea", value = "Der Bürger darf die oben gennante Waffe führen bis zum Ablauf der gültigkeit.", can_be_emtpy = false },
+            { label = "Gueltig bis", type = "input", value = "", can_be_empty = false },
+			      { label = "Waffe", type = "input", value = "", can_be_empty = false },
+            { label = "INFORMATION", type = "textarea", value = "Der Buerger darf die oben gennante Waffe führen bis zum Ablauf der Gueltigkeit.", can_be_emtpy = false },
           }
         },
         {
-          headerTitle = "FÜHRUNGSZEUGNIS",
-          headerSubtitle = "Polizeiliches Führungszeugnis",
+          headerTitle = "Fuehrungszeugnis",
+          headerSubtitle = "Polizeiliches Fuehrungszeugnis",
           elements = {
             { label = "Vorname", type = "input", value = "", can_be_emtpy = false },
             { label = "Nachname", type = "input", value = "", can_be_emtpy = false },
-            { label = "Gültig bis", type = "input", value = "", can_be_empty = false },
-            { label = "Eintragunen", type = "textarea", value = "Der Bürger hat folgende Einträge - ", can_be_emtpy = false, can_be_edited = true },
+            { label = "Gueltig bis", type = "input", value = "", can_be_empty = false },
+            { label = "Eintragunen", type = "textarea", value = "Der Buerger hat folgende Eintraege - ", can_be_emtpy = false, can_be_edited = true },
           }         }
       },
       ["ambulance"] = {
         {
-          headerTitle = "MEDIZINISCHER BERICHT - PATHOLOGIE",
-          headerSubtitle = "Offizieller ärztlicher Bericht eines Pathologen.",
+          headerTitle = "Medizinischer Bericht - PATHOLOGIE",
+          headerSubtitle = "Offizieller aerztlicher Bericht eines Pathologen.",
           elements = {
             { label = "Vorname", type = "input", value = "", can_be_emtpy = false },
             { label = "Nachname", type = "input", value = "", can_be_emtpy = false },
-            { label = "Gültig bis", type = "input", value = "", can_be_empty = false },
-            { label = "MEDIZINISCHER BEFUND", type = "textarea", value = "DER BÜRGER WURDE VON EINEM PATHOLOGEN UNTERSUCHT UND FÜR GESUND ERKLÄRT, OHNE ERKANNTE LANGZEITERKRANKUNG. DIESER BERICHT GILT BIS ZUM GENANNTEN DATUM.", can_be_emtpy = false },
+            { label = "Gueltig bis", type = "input", value = "", can_be_empty = false },
+            { label = "MEDIZINISCHER BEFUND", type = "textarea", value = "DER BUERGER WURDE VON EINEM PATHOLOGEN UNTERSUCHT UND FÜR GESUND ERKLÄRT, OHNE ERKANNTE LANGZEITERKRANKUNG. DIESER BERICHT GILT BIS ZUM GENANNTEN DATUM.", can_be_emtpy = false },
           }
         },
         {
-          headerTitle = "MEDIZINISCHER BERICHT - PSYCHOLOGIE",
-          headerSubtitle = "Offizieller ärztlicher Bericht eines Psychologen.",
+          headerTitle = "Medizinischer Bericht - PSYCHOLOGIE",
+          headerSubtitle = "Offizieller aerztlicher Bericht eines Psychologen.",
           elements = {
             { label = "Vorname", type = "input", value = "", can_be_emtpy = false },
             { label = "Nachname", type = "input", value = "", can_be_emtpy = false },
-            { label = "Gültig bis", type = "input", value = "", can_be_empty = false },
-            { label = "MEDIZINISCHER BEFUND", type = "textarea", value = "DER BÜRGER WURDE VON EINEM PSYCHOLOGEN UNTERSUCHT UND FÜR GESUND ERKLÄRT, OHNE ERKANNTE LANGZEITERKRANKUNG. DIESER BERICHT GILT BIS ZUM GENANNTEN DATUM.", can_be_emtpy = false },
+            { label = "Gueltig bis", type = "input", value = "", can_be_empty = false },
+            { label = "MEDIZINISCHER BEFUND", type = "textarea", value = "DER BUERGER WURDE VON EINEM PSYCHOLOGEN UNTERSUCHT UND FÜR GESUND ERKLÄRT, OHNE ERKANNTE LANGZEITERKRANKUNG. DIESER BERICHT GILT BIS ZUM GENANNTEN DATUM.", can_be_emtpy = false },
           }
         },
         {
-          headerTitle = "MEDIZINISCHER BERICHT - AUGENSPEZIALIST",
-          headerSubtitle = "Offizieller ärztlicher Bericht eines Augenarztes.",
+          headerTitle = "Medizinischer Bericht - AUGENSPEZIALIST",
+          headerSubtitle = "Offizieller aerztlicher Bericht eines Augenarztes.",
           elements = {
             { label = "Vorname", type = "input", value = "", can_be_emtpy = false },
             { label = "Nachname", type = "input", value = "", can_be_emtpy = false },
-            { label = "Gültig bis", type = "input", value = "", can_be_empty = false },
-            { label = "MEDIZINISCHER BEFUND", type = "textarea", value = "DER BÜRGER WURDE VON EINEM AUGENARZT UNTERSUCHT UND FÜR GESUND ERKLÄRT, OHNE ERKANNTE LANGZEITERKRANKUNG. DIESER BERICHT GILT BIS ZUM GENANNTEN DATUM.", can_be_emtpy = false },
+            { label = "Gueltig bis", type = "input", value = "", can_be_empty = false },
+            { label = "MEDIZINISCHER BEFUND", type = "textarea", value = "DER BUERGER WURDE VON EINEM AUGENARZT UNTERSUCHT UND FÜR GESUND ERKLÄRT, OHNE ERKANNTE LANGZEITERKRANKUNG. DIESER BERICHT GILT BIS ZUM GENANNTEN DATUM.", can_be_emtpy = false },
           }
         },
         {
           headerTitle = "Marihuana-Nutzungserlaubnis",
-          headerSubtitle = "Offizielle Erlaubnis zur Verwendung von Marihuana für Bürger.",
+          headerSubtitle = "Offizielle Erlaubnis zur Verwendung von Marihuana für Buerger.",
           elements = {
             { label = "Vorname", type = "input", value = "", can_be_emtpy = false },
             { label = "Nachname", type = "input", value = "", can_be_emtpy = false },
             { label = "Gültig bis", type = "input", value = "", can_be_empty = false },
-            { label = "ERLAUBNIS", type = "textarea", value = "Die rechtliche und zulässige Menge, die der Bürger mitführen kann, darf nicht mehr als 50 Gramm betragen. Nur in Verbindung mit einem ärtzlichen Bericht gültig welcher klärt woran der Bürger leidet.", can_be_emtpy = false, can_be_edited = false },
+            { label = "ERLAUBNIS", type = "textarea", value = "Die rechtliche und zulässige Menge, die der Bürger mitfuehren kann, darf nicht mehr als 50 Gramm betragen. Nur in Verbindung mit einem ärtzlichen Bericht gültig, welcher klärt woran der Bürger leidet. Grund:", can_be_emtpy = false, can_be_edited = false },
           }
         },
 
-      ["avocat"] = {
+      ["cardealer"] = {
         {
-          headerTitle = "LEGAL SERVICES CONTRACT",
-          headerSubtitle = "Legal services contract provided by a lawyer.",
+          headerTitle = "Fahrzeugschein",
+          headerSubtitle = "Rechnung.",
           elements = {
-            { label = "CITIZEN FIRSTNAME", type = "input", value = "", can_be_emtpy = false },
-            { label = "CITIZEN LASTNAME", type = "input", value = "", can_be_emtpy = false },
-            { label = "VALID UNTIL", type = "input", value = "", can_be_empty = false },
-            { label = "INFORMATION", type = "textarea", value = "THIS DOCUMENT IS PROOF OF LEGAL REPRESANTATION AND COVERAGE OF THE AFOREMENTIONED CITIZEN. LEGAL SERVICES ARE VALID UNTIL THE AFOREMENTIONED EXPIRATION DATE.", can_be_emtpy = false },
+            { label = "Verkaeufer", type = "input", value = "", can_be_emtpy = false },
+            { label = "Kaeufer", type = "input", value = "", can_be_emtpy = false },
+            { label = "Kennzeichen", type = "input", value = "", can_be_empty = false },
+            { label = "Information", type = "textarea", value = "Auto:", can_be_emtpy = false },
           }
         },
-		["ballas"] = {
+		["mechanic"] = {
         {
           headerTitle = "Teilezulassung",
           headerSubtitle = "Teilezulassung für Sonderteile am KFZ",
@@ -160,7 +160,7 @@ Config.Documents['de'] = {
             { label = "Vorname", type = "input", value = "", can_be_emtpy = false },
             { label = "Nachname", type = "input", value = "", can_be_emtpy = false },
             { label = "KFZ & Kenzeichen", type = "input", value = "", can_be_empty = false },
-            { label = "Teile Welche Zugelassen und Eingebaut wurden.", type = "textarea", value = "Hier bitte alle Teile und Umbauten Eintragen.", can_be_emtpy = false },
+            { label = "Teile, welche zugelassen und eingebaut wurden.", type = "textarea", value = "Eintrag zu den Teilen:", can_be_emtpy = false },
           }
         }
       }
